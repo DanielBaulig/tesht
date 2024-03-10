20d8f04791b09445a941f934e8d2dc18b37b0577
Daniel Baulig <me@danielbaulig.de>
2024-03-10 15:36:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# tesht 
+
+*The [Swabian Housewife's](https://www.wikidata.org/wiki/Q19307530) Testing Framework*
+
+`tesht` is a minimalist and thrifty shell script testing framework. I had the need
+to test some simple shell scripts and didn't want to pull in testing frameworks
+that are ten to a hrundred times the size of the actual code I am testing, so I
+wrote tesht.
+
+## Usage
+
+```tests/test-mytest.sh
+source ../lib/tesht.sh
+
+../myscript
+assert "../myscript exits without an error code"
+```
+
+```
+> lib/tesht.sh tests/
+```
+
+## Features
+
+### Return code assertions
+
+The core building block for all assertions in tesht is a generic return code
+assertion `assert`.
+
+```
+source ../lib/tesht.sh
+
+true
+assert "Will pass"
+
+false
+assert "Will fail"
+
+[[ -f "file.txt" ]]
+assert "Will fail if file.txt doesn't exist"
+```
+
+### Mocks
+tesht will help you with mocking and tracking calls to functions and other
+commands using it's simple mock implementation.
+
+```
+source ../lib/tesht.sh
+
+function mock_ip_impl() {
+    # By defining a mock implementation we can control the behavior of our mock
+    case "$@" in
+        "route")
+            # myscript_using_ip.sh will query ip route to see if a specific
+            # route exists. So echo a table without that specific route
+            echo "default via 10.0.0.1 dev eth0 proto kernel"
+            ;;
+    esac
+    # All other invokcations will not print anything
+}
+
+ip_mock=`make_mock`
+mock $ip_mock ip mock_ip_impl
+
+../myscript_using_ip.sh
+
+# Make sure a new route was added by myscript_using_ip.sh
+assert_called_with $ip_mock "route" "add" "192.168.0.0/24"
---
+++ tesht.sh
@@ -0,0 +1,157 @@
+#!/bin/sh
+
+export TEST_FILE=$0
+
+if [[ "`basename $0`" = "tesht.sh" ]]; then
+    declare -i failed_tests=0
+    echo "tesht - the swabian housewife's testing framework"
+    echo Running tests...
+
+    for t in ${1:-tests}/test-*; do
+        [[ -x "$t" ]] && echo && $t
+        if [[ $? -gt 0 ]]; then
+            failed_tests+=1
+        fi
+    done
+
+    exit $failed_tests
+fi
+
+MOCKS+=()
+declare -gi assertions=0
+declare -i failures=0
+
+__gray() {
+    echo "\e[90m$*\e[0m"
+}
+
+__red() {
+    echo "\e[31m$*\e[0m"
+}
+
+__green() {
+    echo "\e[32m$*\e[0m"
+}
+
+echo -e "`__gray $TEST_FILE`"
+
+
+__cleanup() {
+    if [[ $failures -eq 0 ]]; then
+        echo -e "`__green SUCCESS` ($assertions/$assertions assertions passed)"
+    else 
+        echo -e "`__red FAILURE` (`expr $assertions - $failures`/$assertions assertions passed)"
+    fi
+
+    for mock in ${MOCKS[*]} ; do
+        [[ -f $mock ]] && rm $mock
+    done
+
+    exit $failures
+}
+
+trap "__cleanup" EXIT
+
+contains() {
+    grep -- "$1" >/dev/null
+}
+
+__print_reference() {
+    caller $1 | awk '{ print $3":"$1 }'
+}
+
+__fail() {
+    failures+=1
+    local -i stack_depth=`expr $2 + 1`
+    local reference=`__print_reference $stack_depth`
+    echo -e "`__red ✗` $1\n`__gray $reference`" 1>&2
+
+}
+
+__assert() {
+    local result=$?
+    assertions+=1
+    if [ ! "$result" -eq 0 ]
+    then
+        __fail "$1" 2
+    fi
+}
+
+assert() {
+    __assert "$1"
+}
+
+assert_fail() {
+    false
+    __assert "$1"
+}
+
+assert_contains() {
+    local input=`cat`
+    echo "$input" | contains "$1"
+    __assert "\"$input\" contains \"$1\""
+}
+
+mock_create() {
+    mktemp
+}
+
+__mock_trace() {
+    echo "${*:2}" >> "$1"
+}
+
+make_mock() {
+    local mock=`mktemp`
+    echo $mock
+}
+
+__noop() {
+    :
+}
+
+mock() {
+    local mock="$1"
+    local name="$2"
+    local mock_fn="${3:-"__noop"}"
+    local impl="function $name() { __mock_trace \"$mock\" \"\$@\"; $mock_fn \$@; }"
+    eval $impl
+    export -f "$name" "$mock_fn"
+
+    MOCKS+=("$mock")
+    echo $name > $mock
+}
+
+mock_name() {
+    head -n 1 "$1"
+}
+
+mock_calls() {
+    tail -n +2 "$1"
+}
+
+mock_reset() {
+    local mock=$1
+    local name=`mock_name "$mock"`
+    echo "$name" > "$mock"
+}
+
+assert_called() {
+    local name=`mock_name "$1"`
+    [[ "`mock_calls "$1" | wc -l`" -gt 0 ]]
+    __assert "$name was called"
+}
+
+assert_called_with() {
+    local mock="$1"
+    local name=`mock_name "$mock"`
+    local calls=`mock_calls "$mock"`
+    echo "$calls" | while IFS= read -r call ; do 
+        (for arg in "${@:2}" ; do
+            echo "$call" | contains "$arg" || exit 1
+        done) && break
+        false
+    done
+    __assert "$name was called with args\nExpected:\n${*:2}\nActual:\n$calls"
+}
+
+export -f __mock_trace
